6f6dcb973d5d3a97cb4f33e91f31a4ac617be9b3
Eric Greene <eric@t4d.io>
2020-08-25 18:23:04 -0400

    add exercise 3

--- react-app/src/components/CarTool.js
+++ react-app/src/components/CarTool.js
@@ -3,9 +3,41 @@ import React from 'react';
 export const CarTool = () => {
 
   return (
-    <header>
-      <h1>Car Tool</h1>
-    </header>
+    <>
+      <header>
+        <h1>Car Tool</h1>
+      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Make</th>
+            <th>Model</th>
+            <th>Year</th>
+            <th>Color</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Ford</td>
+            <td>Fusion Hybrid</td>
+            <td>2019</td>
+            <td>white</td>
+            <td>45000</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Tesla</td>
+            <td>S</td>
+            <td>2019</td>
+            <td>red</td>
+            <td>120000</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
   );
 
 };
--- react-app/src/components/ColorTool.js
+++ react-app/src/components/ColorTool.js
@@ -2,16 +2,21 @@ import React from 'react';
 
 export const ColorTool = () => {
 
+  const colors = [
+    { id: 1, name: 'blue' },
+    { id: 2, name: 'green' },
+    { id: 3, name: 'orange' },
+    { id: 4, name: 'purple' },
+  ];
+
   return (
     <>
       <header>
         <h1>Color Tool</h1>
       </header>
       <ul>
-        <li>blue</li>
-        <li>green</li>
-        <li>orange</li>
-        <li>purple</li>
+        {colors.map(color =>
+          <li key={color.id}>{color.name}</li>)}
       </ul>
     </>
   );

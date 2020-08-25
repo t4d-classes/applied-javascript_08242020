05809ac012c7676255a32a813fcb426a0bc3c1df
Eric Greene <eric@t4d.io>
2020-08-25 17:15:12 -0400

    add exercise 1

--- learn-js/js/app.js
+++ learn-js/js/app.js
@@ -2,22 +2,59 @@
 
 const randomDelay = () => Math.floor(Math.random() * 1000) + 1000;
 
-const xhr = new XMLHttpRequest();
+const myFetch = (url) => {
 
-xhr.addEventListener('readystatechange', () => {
+  return new Promise(resolve => {
 
-  if (xhr.status === 200 && xhr.readyState === 4) {
-    console.log(JSON.parse(xhr.responseText));
-  }
+    const xhr = new XMLHttpRequest();
 
-});
+    xhr.addEventListener('readystatechange', () => {
+    
+      if (xhr.status === 200 && xhr.readyState === 4) {
+        resolve(JSON.parse(xhr.responseText));
+      }
+    
+    });
+    
+    xhr.open('GET', url);
+    xhr.send();
 
-xhr.open('GET', 'http://localhost:5050/colors');
-xhr.send();
+  });
+
+};
+
 
 // Exercise
 
 // Create a function named 'myFetch' using the XHR code above to implement the following
 // API call
 
-myFetch('http://localhost:5050/colors').then(colors => console.log(colors));
+// myFetch('http://localhost:5050/colors').then(colors => console.log(colors));
+
+// Promise.all([
+//   fetch('http://localhost:5050/colors').then(res => res.json()),
+//   fetch('http://localhost:5050/cars').then(res => res.json())
+// ]).then(results => {
+//   console.log(results);
+// });
+
+// fetch('http://localhost:5050/colors')
+//   .then(res => res.json())
+//   .then(colors => console.log(colors))
+//   .then(() => fetch('http://localhost:5050/cars'))
+//   .then(res => res.json())
+//   .then(cars => console.log(cars));
+
+const getData = async () => {
+
+  const res = await fetch('http://localhost:5050/colors');
+  const colors = await res.json();
+  console.log(colors);
+
+  const res2 = await fetch('http://localhost:5050/cars');
+  const cars = await res2.json();
+  console.log(cars);
+
+};
+
+getData().then(() => console.log('all done'));

9080180c0ca06b149f557e5ed8560def27c9529f
Eric Greene <eric@t4d.io>
2020-08-27 15:09:02 -0400

    add code demos

---
+++ angular-app/exercises/exercise-4.md
@@ -0,0 +1,11 @@
+# Exercise 4
+
+1. Utilize the new Tool Header component within Car Tool. Replace the header in Car Home with the Tool Header component.
+
+2. Create a new component named Car Table within the Car Tool module. Move the car table from Car Home to the Car Table component. Setup the appropriate inputs to pass in the cars.
+
+3. Utilize the new Car Table component within Car Home.
+
+4. Create a new component named Car View Row. The car view row component will display a row of car data. Utilize the Car View Row component within Car Table.
+
+5. Ensure it works!
--- angular-app/src/app/color-tool/color-tool.module.ts
+++ angular-app/src/app/color-tool/color-tool.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { SharedModule } from '../shared/shared.module';
 import { ColorHomeComponent } from './components/color-home/color-home.component';
 
 
@@ -9,7 +10,7 @@ import { ColorHomeComponent } from './components/color-home/color-home.component
 @NgModule({
   declarations: [ColorHomeComponent],
   imports: [
-    CommonModule, ReactiveFormsModule,
+    CommonModule, ReactiveFormsModule, SharedModule,
   ],
   exports: [ ColorHomeComponent ],
 })

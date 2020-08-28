199fc463b072c9f5eebfd8cef19357b2d11bd1a6
Eric Greene <eric@t4d.io>
2020-08-28 14:44:04 -0400

    add one zip endpoint

--- node-full-stack-app/server-app/routers/ZipRouter.js
+++ node-full-stack-app/server-app/routers/ZipRouter.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const { ObjectID } = require('mongodb');
 
 const { Zip } = require('../models/Zip');
 const { logger } = require('../logger');
-
 
 const ZipRouter = express.Router();
 
@@ -17,6 +17,34 @@ ZipRouter.route('/')
       res.sendStatus(500);
     }
 
+  });
+
+ZipRouter.route('/:id')
+  .get(async (req, res) => {
+
+    try {
+
+      if (!ObjectID.isValid(req.params.id)) {
+        logger.error('zip object id is invalid: ' + req.params.id);
+        res.sendStatus(400);
+        return;
+      }
+
+      const zip = await Zip.findById(req.params.id);
+
+      if (zip == null) {
+        logger.error('zip object with id ' + req.params.id + ' was not found');
+        res.sendStatus(404);
+        return;
+      }
+
+      res.json(zip);
+
+    } catch (err) {
+      logger.error(err);
+      res.sendStatus(500);
+    }    
+  
   });
 
 module.exports.ZipRouter = ZipRouter;
